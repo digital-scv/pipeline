85dbc17496d9e82540f99771f88521ba7ac5ebbc
ji-gyoomin <earth1223@sk.com>
2018-05-29 17:06:07 +0900

    parameter delegate

---
+++ src/retort/utils/Utils.groovy
@@ -0,0 +1,41 @@
+package retort.utils
+
+class Utils {
+
+  static def delegateParameters(ret, defaultConfig) {
+    def config = [:]
+    if (defaultConfig) {
+      if (defaultConfig instanceof Map) {
+        config = defaultConfig
+      } else if (defaultConfig instanceof Closure) {
+        defaultConfig.resolveStrategy = Closure.DELEGATE_FIRST
+        defaultConfig.delegate = config
+        defaultConfig()
+      }
+    }
+    
+    if (ret instanceof Map) {
+        config.putAll(ret)
+    } else if (ret instanceof Closure) {
+        ret.resolveStrategy = Closure.DELEGATE_FIRST
+        ret.delegate = config
+        ret()
+    }
+    
+    return config
+  }
+  
+  static def delegateParameters(ret) {
+    def config = [:]
+    
+    if (ret instanceof Map) {
+        config.putAll(ret)
+    } else if (ret instanceof Closure) {
+        ret.resolveStrategy = Closure.DELEGATE_FIRST
+        ret.delegate = config
+        ret()
+    }
+    
+    return config
+  }
+}
