527a773916adadb1337eb5944b17010680eb6bb9
ji-gyoomin <earth1223@sk.com>
2018-05-30 16:54:20 +0900

    Update dockerCmd

---
+++ vars/dockerCmd.groovy
@@ -0,0 +1,23 @@
+import retort.utils.logging.Logger
+import static retort.utils.Utils.delegateParameters as getParam
+
+def build(ret) {
+  Logger logger = logger.getLogger(this)
+  def config = getParam(ret) {
+    path = '.'
+  }
+  
+  def command = new StringBuffer()
+  command.append('docker buid')
+  
+  if (config.tag) {
+    logger.debug("TAG : ${config.tag}")
+    command.append(" -t ${config.tag}")
+  }
+
+  if (config.path) {
+    command.append(" ${config.path}")
+  }
+  
+  sh command.toString()
+}
