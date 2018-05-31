9d4c57a325ad616fdb8f845925886adbb2de4b00
ji-gyoomin <earth1223@sk.com>
2018-05-31 11:38:26 +0900

    Update dockerCmd

--- vars/dockerCmd.groovy
+++ vars/dockerCmd.groovy
@@ -12,6 +12,7 @@ def build(ret) {
   
   appendCommand(config, 'file', '-f', command, logger)
   setTag(command, config, logger)
+  setBuildArgs(command, config, map)
   appendCommand(config, 'options', '', command, logger)
   appendCommand(config, 'path', '', command, logger)
   
@@ -21,6 +22,24 @@ def build(ret) {
 def tag(ret) {
   Logger logger = Logger.getLogger(this)
 }
+
+private def setBuildArgs(command, config, logger) {
+  if (!config.buildArgs) {
+    return
+  }
+
+  if (config.buildArgs instanceof Map) {
+    command.append(config.buildArgs.collect { k, v ->
+      logger.debug("[BUILD ARG] ${k} : ${v}")
+      return " -build-arg ${k}=${v}"
+    }.join())
+  } else {
+      logger.error("buildArgs option only supports Map type parameter.")
+      logger.error("example : ['key1':'value1','key2':'value2']")
+      throw new Exception('buildArgs option only supports Map type parameter.')
+    }
+}
+
 
 private def setTag(command, config, logger) {
   if (!config.tag) {
