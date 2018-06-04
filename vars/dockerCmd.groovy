1b72683675d8c94dba8c159811e374e32f11fb04
ji-gyoomin <earth1223@sk.com>
2018-06-04 17:23:03 +0900

    Update dockerCmd

--- vars/dockerCmd.groovy
+++ vars/dockerCmd.groovy
@@ -77,14 +77,15 @@ def tag(ret) {
 private def pushWithCredentialId(config, command, logger) {
   def loginCommand
   logger.debug("Login with jenkins credential : ${config.credentialId}")
-  if (config.registry) {
-    logger.debug("Registry : ${config.registry}")
-    loginCommand = "docker login ${config.registry} -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}"
-  } else {
-    logger.debug("Registry : docker.io")
-    loginCommand = "docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}"
-  }
   withCredentials([usernamePassword(credentialsId: config.credentialId, passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USER')]) {
+    if (config.registry) {
+      logger.debug("Registry : ${config.registry}")
+      loginCommand = "docker login ${config.registry} -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}"
+    } else {
+      logger.debug("Registry : docker.io")
+      loginCommand = "docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}"
+    }
+
     sh """
       ${loginCommand}
       ${command}
