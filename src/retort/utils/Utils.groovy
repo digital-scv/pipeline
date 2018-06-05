cc3f3904093f2c6bb373e33163735f6b0a33d283
ji-gyoomin <earth1223@sk.com>
2018-06-05 18:11:57 +0900

    add file methods

--- src/retort/utils/Utils.groovy
+++ src/retort/utils/Utils.groovy
@@ -1,7 +1,13 @@
 package retort.utils
 
+/**
+ * Utility class.
+ */
 class Utils {
 
+  /**
+   * parameter to map with default values.
+   */
   static def delegateParameters(ret, defaultConfig) {
     def config = [:]
     if (defaultConfig) {
@@ -25,6 +31,9 @@ class Utils {
     return config
   }
   
+  /**
+   * parameter to map.
+   */
   static def delegateParameters(ret) {
     def config = [:]
     
@@ -38,4 +47,40 @@ class Utils {
     
     return config
   }
+  
+  /**
+   * get Filename from full path.
+   */
+  static def getFilename(fileWithPath) {
+    return fileWithPath.lastIndexOf(File.separator).with {it != -1 ? fileWithPath.substring(it+1) : fileWithPath}
+  }
+  
+  /**
+   * get file extension from full path.
+   */
+  static def getFileExtension(fileWithPath) {
+    def filename = getFilename(fileWithPath)
+    return fileWithPath.lastIndexOf('.').with {it != -1 ? fileWithPath.substring(it+1) : fileWithPath}
+  }
+
+  /**
+   * return file has yaml extension or not.
+   */
+  static def isYamlfile(fileWithPath) {
+    def yamlExtensionList = ['yaml', 'yml']
+    def extension = getFileExtension(fileWithPath).toLowerCase()
+    
+    return yamlExtensionList.contains(extension)
+  }
+  
+  /**
+   * return file has json extension or not.
+   */
+  static def isJsonfile(fileWithPath) {
+    def jsonExtensionList = ['json']
+    def extension = getFileExtension(fileWithPath).toLowerCase()
+    
+    return jsonExtensionList.contains(extension)
+  }
+
 }
