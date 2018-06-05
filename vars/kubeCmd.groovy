1b645df23f0d61d3bf35d7a83adc64251891d536
ji-gyoomin <earth1223@sk.com>
2018-06-05 13:30:00 +0900

    Add kubeCmd

---
+++ vars/kubeCmd.groovy
@@ -0,0 +1,49 @@
+import retort.utils.logging.Logger
+import static retort.utils.Utils.delegateParameters as getParam
+
+/**
+ * kubectl apply
+ *
+ * @param
+ */
+def apply(ret) {
+  Logger logger = Logger.getLogger(this)
+  def config = getParam(ret)
+}
+
+/**
+ * kubectl describe
+ *
+ * @param type resource type. ex) deploy, service etc.
+ * @param name resource name or name prefix
+ * @param file resource yaml file
+ * @param throwException : false
+ */
+def describe(ret) {
+  Logger logger = Logger.getLogger(this)
+  def config = getParam(ret)
+  
+  def command = new StringBuffer('kubectl describe')
+  
+  if (config.type && config.name) {
+    command.append(" ${config.type}")
+    command.append(" ${config.name}")
+  } else if (config.file) {
+    command.append(" -f ${config.file}")
+  } else {
+    logger.debug('type and name values are required. or specify file value.')
+    if (config.throwException == true) {
+      throw new IllegalArgumentException('type and name values are required. or specify file value.');
+    }
+    return
+  }
+  
+  try {
+    sh command.toString()
+  } catch (Exception e) {
+    if (config.throwException == true) {
+      throw e
+    }
+  }
+
+}
