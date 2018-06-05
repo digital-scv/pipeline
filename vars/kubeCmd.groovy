20201d0adf6fb3c2d36fef3947e721244297301c
ji-gyoomin <earth1223@sk.com>
2018-06-05 18:12:13 +0900

    Add getValue method

--- vars/kubeCmd.groovy
+++ vars/kubeCmd.groovy
@@ -4,7 +4,10 @@ import static retort.utils.Utils.delegateParameters as getParam
 /**
  * kubectl apply
  *
- * @param file
+ * @param file resource file. YAML or json
+ * @param namespace 
+ * @param option apply option
+ * @param wait Wait n seconds while this resource rolled out
  */
 def apply(ret) {
   Logger logger = Logger.getLogger(this)
@@ -22,9 +25,7 @@ def apply(ret) {
     logger.debug("OPTION : ${config.option}")
     command.append(" ${config.option}")
   }
-
   
-
   logger.debug('apply executed')
 }
 
@@ -33,6 +34,7 @@ def apply(ret) {
  *
  * @param type resource type. ex) deploy, service etc.
  * @param name resource name or name prefix
+ * @param label label selector array
  * @param file resource yaml file
  * @param namespace namespace
  * @param throwException : false throw Exception 
@@ -90,4 +92,81 @@ def describe(ret) {
 
 }
 
+/**
+ * kubectl get with json path
+ *
+ * @param file
+ * @param type
+ * @param name
+ * @param jsonpath 
+ * @param namespace
+ * @param throwException : false throw Exception 
+ * @return String value
+ */
+def getValue(ret) {
+  Logger logger = Logger.getLogger(this)
+  def config = getParam(ret)
+  
+  def value = ''
+  def command = new StringBuffer('kubectl get')
+  
+  if (config.type && config.name) {
+    logger.debug("RESOURCE TYPE : ${config.type}")
+    command.append(" ${config.type}")
+    
+    // kubectl get type name
+    logger.debug("RESOURCE NAME : ${config.name}")
+    command.append(" ${config.name}")
+  } else if (config.file) {
+    logger.debug("RESOURCE FILE : ${config.file}")
+    command.append(" -f ${config.file}")
+  } else {
+    logger.error('type and name values are required. or specify file value.')
+    if (config.throwException == true) {
+      throw new IllegalArgumentException('type and name values are required. or specify file value.')
+    }
+    return value
+  }
+  
+  if (config.namespace) {
+    logger.debug("NAMESPACE : ${config.namespace}")
+    command.append(" -n ${config.namespace}")
+  }
+  
+  if (config.jsonpath) {
+    logger.debug("JSONPATH : ${config.jsonpath}")
+    command.append(" -o jsonpath=${config.jsonpath}")
+  } else {
+    logger.error('jsonpath value is required.')
+    if (config.throwException == true) {
+      throw new IllegalArgumentException('jsonpath value is required.')
+    }
+    return value
+  }
+  
+  try {
+    value = sh script: command.toString(), returnStdout: true
+  } catch (Exception e) {
+    if (config.throwException == true) {
+      throw e
+    }
+  }
+  
+  return value
+}
+
+
+/**
+ * excute apply command.
+ */
+private def executeApply(command, config, logger) {
+  
+  if (config.wait) {
+    sh "kubectl "
+  } else {
+    
+  }
+
+
+}
 
