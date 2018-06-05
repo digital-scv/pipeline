85edfcea672214a244a8cb23aca97d4dd2ecf4d1
ji-gyoomin <earth1223@sk.com>
2018-06-05 15:09:06 +0900

    test

--- vars/kubeCmd.groovy
+++ vars/kubeCmd.groovy
@@ -28,7 +28,32 @@ def describe(ret) {
   
   logger.debug('describe executed')
   
+  def command = new StringBuffer('kubectl describe')
   
+  if (config.type && config.name) {
+    command.append(" ${config.type}")
+    command.append(" ${config.name}")
+  } else if (config.file) {
+    command.append(" -f ${config.file}")
+  } else {
+    logger.debug('type and name values are required. or specify file value.')
+    if (config.throwException == true) {
+      throw new IllegalArgumentException('type and name values are required. or specify file value.')
+    }
+    return
+  }
+  
+  if (config.namespace) {
+    command.append(" -n ${config.namespace}")
+  }
+  
+  try {
+    sh command.toString()
+  } catch (Exception e) {
+    if (config.throwException == true) {
+      throw e
+    }
+  }
 
 }
 
