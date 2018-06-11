211758654983a934ad3d522c763865c33c5a0c32
ji-gyoomin <earth1223@sk.com>
2018-06-11 18:14:30 +0900

    Add Exception Module

---
+++ src/retort/utils/MessageUtil.groovy
@@ -0,0 +1,46 @@
+package retort.utils
+
+import java.text.MessageFormat
+import retort.utils.logging.Logger
+
+class MessageUtil {
+
+
+  private static boolean initialize = false
+
+  private static Map messageSource
+
+  public static boolean isInitialized() {
+    return initialize
+  }
+
+
+  @NonCPS
+  public static String getMessage(String code) {
+    def message = messageSource[code]
+    
+    if (message) {
+      return message
+    }
+
+    return code
+  }
+
+  @NonCPS
+  public static String getMessage(String code, String... args) {
+    
+    def messageFormat = messageSource[code]
+    
+    if (messageFormat) {
+      return MessageFormat.format(messageFormat, args)
+    }
+
+    return code
+  }
+  
+  public static void setMessages(messages) {
+    messageSource = messages
+  }
+
+
+}
---
+++ vars/createException.groovy
@@ -0,0 +1,50 @@
+import retort.utils.logging.Logger
+import retort.utils.MessageUtil
+import retort.exception.RetortException
+
+def call(errorCode) {
+  Logger logger = Logger.getLogger(this)
+  init(logger)
+  return new RetortException(errorCode)
+}
+
+
+def call(errorCode, String... args) {
+  Logger logger = Logger.getLogger(this)
+  init(logger)
+  return new RetortException(errorCode, args)
+}
+
+def call(errorCode, Throwable throwable) {
+  Logger logger = Logger.getLogger(this)
+  init(logger)
+  return new RetortException(errorCode, throwable)
+}
+
+def call(errorCode, Throwable throwable, String... args) {
+  Logger logger = Logger.getLogger(this)
+  init(logger)
+  return new RetortException(errorCode, throwable, args)
+}
+
+
+private init(logger) {
+  if (MessageUtil.isInitialized()) {
+    return
+  }
+
+  logger.debug('MessageUtil initialize')
+  
+  def r = "message/errorMessage.properties"
+    
+  def errorMessage = libraryResource r
+    
+  def messageSource = readProperties text: errorMessage
+  
+  if (messageSource == null) {
+    messageSource = [:]
+  }
+
+  MessageUtil.setMessages(messageSource) 
+}
+
