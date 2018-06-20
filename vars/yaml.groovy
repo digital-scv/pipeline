a3358e899911a0fe174248f2b34e437140d7b61d
ji-gyoomin <earth1223@sk.com>
2018-06-20 13:53:01 +0900

    yamlUpdate

--- vars/yaml.groovy
+++ vars/yaml.groovy
@@ -1,6 +1,9 @@
 @Grab('org.yaml:snakeyaml:1.17')
 import org.yaml.snakeyaml.Yaml as Parser
 
+import retort.utils.logging.Logger
+import static retort.utils.Utils.delegateParameters as getParam
+
 //Expose static function like 'yaml.load()'
 //- https://stackoverflow.com/a/25603288
 static def load(String yaml){
@@ -11,4 +14,56 @@ static def load(String yaml){
 
 static def dump(def yaml){
   new Parser().dump(yaml)
+}
+
+/**
+ * file
+ * update map key: yaml path, value: value
+ */
+def update(def ret) {
+  Logger logger = Logger.getLogger(this)
+  def config = getParam(ret)
+  
+  if (!config.file) {
+    logger.error('file is required.')
+    createException('RC401')
+  }
+  
+  logger.debug("FILE : ${config.file}")
+ 
+  if (!config.update) {
+    logger.error('file is required.')
+    createException('RC402')
+  } else if (!(config.update in Map)){
+    logger.error('update only support Map type parameter.')
+    createException('RC403')
+  }
+  
+  logger.debug("UPDATE : ${config.update}")
+
+  def yamlText = readFile file:'k8s/deploy.yaml'
+  logger.debug("""
+Original yaml contents
+${yamlText}
+""")
+
+  def yaml = load(yamlText)
+  
+  update.each { k, v ->
+      def binding = new Binding(yaml:yaml)
+      def shell = new GroovyShell(binding)
+      def expression = "yaml${k} = '${v}'"
+      logger.debug 
+      
+      shell.evaluate(expression)
+  }
+  
+  def updatedYamlText = dump(yaml)
+  
+  logger.debug("""
+Updated yaml contents
+${updatedYamlText}
+""")
+
+  writeFile file: config.file, text: updatedYamlText
 }
