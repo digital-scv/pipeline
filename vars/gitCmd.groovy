8c9a735e90d93ce86ff2a27b38f9fa3f768f05cf
ji-gyoomin <earth1223@sk.com>
2018-08-20 21:37:13 +0900

    Update comment.

--- vars/gitCmd.groovy
+++ vars/gitCmd.groovy
@@ -84,7 +84,7 @@ BUIlD_URL : ${env.BUILD_URL}\"
 
 /**
  * @param gitUrl
- * @param credentialsId required.
+ * @param credentialsId
  * @param tags : false
  */
 def push(ret) {
@@ -141,7 +141,7 @@ def push(ret) {
 /**
  * @param tag env.VERSION
  * @param message
- * @param credentialsId required.
+ * @param credentialsId
  */
 def tag(ret) {
   Logger logger = Logger.getLogger(this)
